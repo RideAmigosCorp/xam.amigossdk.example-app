9c3d1c281e611f4455b123c160b83a3109778ea0
Ben Dalton <bendalton@rideamigos.com>
2021-08-23 13:35:41 -0400

    adding page context example

--- AmigoExampleApp/AmigoExampleApp/MainPage.xaml.cs
+++ AmigoExampleApp/AmigoExampleApp/MainPage.xaml.cs
@@ -5,14 +5,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using AmigosSDK.Abstractions;
+using System.Dynamic;
 
 namespace AmigoExampleApp
 {
     public partial class MainPage : ContentPage
     {
+        public String amigosJWTString;
+        public RAWebViewContext pageContext;
+
         public MainPage()
         {
             InitializeComponent();
+
+            dynamic metadata = new ExpandoObject();
+            metadata.lastPage = "home page";
+
+            amigosJWTString = "some jwt string";
+
+            pageContext = new RAWebViewContext
+            {
+                action = "Main Menu",
+                metadata = metadata
+            };
+
+            //RideAmigosContent.Source = "http://localhost:3000/workplace-app-preview";
+            RideAmigosContent.PageContext = pageContext;
+            RideAmigosContent.JWTString = amigosJWTString;
+
         }
     }
 }
