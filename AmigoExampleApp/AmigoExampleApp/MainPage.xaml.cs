bb19f56bdae52bcc01a5845f1e2d81bb3d5c8da1
Ben Dalton <bendalton@rideamigos.com>
2021-10-28 00:49:20 -0400

    Add example of Android Back Button support

--- AmigoExampleApp/AmigoExampleApp/MainPage.xaml.cs
+++ AmigoExampleApp/AmigoExampleApp/MainPage.xaml.cs
@@ -30,10 +30,35 @@ namespace AmigoExampleApp
                 metadata = metadata
             };
 
-            //RideAmigosContent.Source = "http://localhost:3000/workplace-app-preview";
+            // Uncomment this to point the SDK to a specific instance of the web app
+            //RideAmigosContent.Source = "http://10.0.2.2:3000/workplace-app-preview";
             RideAmigosContent.PageContext = pageContext;
             RideAmigosContent.JWTString = amigosJWTString;
+            RideAmigosContent.OnCloseRequested += OnCloseSDK;
+        }
 
+        /// <summary>
+        /// An example event handler that demonstrates what happens when the SDK requests "close"
+        /// This will need to be implemented within the embedding application
+        /// to fully support the hardware back button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async private void OnCloseSDK(object sender, EventArgs e)
+        {
+            await DisplayAlert("Close Requested", "The SDK has requested to close.", "OK");
+        }
+
+        /// <summary>
+        /// This is an example of overriding the default back button behavior and sending the
+        /// back request to the SDK instead. This will need to be implemented within the embedding
+        /// application in order to fully support the hardware back button.
+        /// </summary>
+        /// <returns></returns>
+        protected override bool OnBackButtonPressed()
+        {
+            RideAmigosContent.BackPressed();
+            return true;
         }
     }
 }
